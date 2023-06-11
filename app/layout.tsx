4491218fd06f214b0c4fd6f0b45933eaece036f7
Zac Godinez <zac.godinez@tutanota.com>
2023-06-12 04:01:37 +1000

    added theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,94 @@
-import './globalStyle.css'
+import "./globalStyle.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import { clsx } from "clsx";
+import { type Metadata } from "next";
+import { siteConfig } from "@/config/site";
+
+import { fontSans } from "@/lib/fonts";
+import { ThemeProvider } from "@/components/theme-provider";
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+  ],
+  authors: [
+    {
+      name: "",
+      url: "",
+    },
+  ],
+  creator: "",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  // manifest: `${siteConfig.url}/site.webmanifest`,
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={clsx(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <div className="relative flex min-h-screen flex-col">
+              {/* <SiteHeader /> */}
+              <div className="flex-1">{children}</div>
+              {/* <SiteFooter /> */}
+            </div>
+          </ThemeProvider>
+          {/* <StyleSwitcher />
+          <Analytics />
+          <Toaster /> */}
+        </body>
+      </html>
+    </>
+  );
 }
---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "z3us",
+  url: "https://z3us.com/",
+  ogImage: "https://ui.shadcn.com/og.jpg",
+  description: "WOW ",
+  links: {
+    twitter: "https://twitter.com/",
+    github: "https://github.com/",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
