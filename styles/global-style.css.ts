6b6535a9301a5d33abc4c1a666b4d9a1699f8988
Zac Godinez <zac.godinez@tutanota.com>
2023-06-13 08:11:09 +1000

    global styles testing

--- styles/global-style.css.ts
+++ styles/global-style.css.ts
@@ -2,10 +2,13 @@ import { globalStyle } from "@vanilla-extract/css";
 
 globalStyle("html, body", {
   height: "100%",
+  margin: 0,
 });
 
 globalStyle("*, *::before, *::after", {
   boxSizing: "border-box",
+  margin: 0,
+  padding: 0,
   WebkitFontSmoothing: "antialiased",
   MozOsxFontSmoothing: "grayscale",
 });
@@ -18,10 +21,13 @@ globalStyle("body", {
     "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
   background: "#000",
   color: "#fff",
-  padding: "0 16px",
-  backgroundSize: "cover",
-  backgroundPosition: "center center",
-  backgroundRepeat: "no-repeat",
+});
+
+globalStyle("::selection", {
+  background: "#BF40BF",
+  color: "#fff",
+  // background: vars.color.purple500,
+  // color: vars.color.white,
 });
 
 globalStyle("h1, h2, h3, h4, p", {
@@ -39,3 +45,18 @@ globalStyle("main", {
   maxWidth: "720px",
   margin: "0 auto",
 });
+
+globalStyle("*, *::before, *::after", {
+  boxSizing: "border-box",
+  margin: 0,
+  padding: 0,
+});
+
+// globalStyle('body', {
+// 	textRendering: 'optimizeLegibility',
+// 	fontFamily: vars.fonts.body,
+// })
+
+// globalStyle(`body.${darkMode}`, {
+// 	colorScheme: 'dark',
+// })
